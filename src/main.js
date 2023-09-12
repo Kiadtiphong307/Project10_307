088f7782253b2e4754d9315408c8222f78320a3c
kiadtiphong192 <kiadtiphong192@gmail.com>
2023-09-12 09:10:33 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+//import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+app.use(BootstrapVue)
+app.use(BootstrapVueIcons)
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
